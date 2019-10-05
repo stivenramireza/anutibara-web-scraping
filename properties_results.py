32d84a6c35f14010bc2dc1052237c1f301a0ac52
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-10-05 11:57:28 -0500

    Updating Properties Results URLs

--- properties_results.py
+++ properties_results.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 paginator_url = 'https://www.fincaraiz.com.co/finca-raiz/venta/medellin/?ad=30|1||||1||8,9,3,4,22,2,5,7,19,23,21,18,20|||55|5500006||||||||||||||||1|||1||griddate%20desc||||-1||'
 page = requests.get(paginator_url)
 soup = BeautifulSoup(page.content, 'html.parser')
+properties_number = soup.find(id='lblNumInm').get_text()
 division = soup.find(id='divAdverts')
 properties_pages_list = []
 for i in range(0, 34):
@@ -17,5 +18,4 @@ for i in range(0, 34):
         property = properties.find('li', class_='media').attrs.get('onclick')
     url_property = str(property).replace('javascript:window.location=', '')
     url_property = 'https://www.fincaraiz.com.co' + url_property.replace("'", "")
-    print('Property #' + str(i) + ": " + str(url_property))
-    #properties_pages_list.append(url_property)
+    properties_pages_list.append(url_property)
