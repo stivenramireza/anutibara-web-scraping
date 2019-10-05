174cdb748e07490f6bb49c4eb9d5f0d1d2bdec20
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-10-05 09:21:29 -0500

    Paginator Results finished

--- paginator_results.py
+++ paginator_results.py
@@ -1,5 +1,17 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
 def paginate_properties(pages_list_to_process):
-    for i in range(1, 839):
+    main_page_url = 'https://www.fincaraiz.com.co/finca-raiz/venta/medellin/?ad=30|1||||1||8,9,3,4,22,2,5,7,19,23,21,18,20|||55|5500006||||||||||||||||1|||1||griddate%20desc||||-1||'
+    page = requests.get(main_page_url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    pattern = re.compile("var totalPages = [\d]*;")
+    total_pages = ''
+    for script in soup.find_all("script", type="text/javascript"):
+        if(pattern.findall(script.text)):
+            total_pages = int((pattern.findall(script.text)[0].split()[-1])[0:-1])
+    for i in range(1, total_pages + 1):
         pages_list_to_process.append('https://www.fincaraiz.com.co/finca-raiz/venta/medellin/?ad=30|'+ str(i) +'||||1||8,9,3,4,22,2,5,7,19,23,21,18,20|||55|5500006||||||||||||||||1|||1||griddate%20desc||||-1||')
     return pages_list_to_process
 
