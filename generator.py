0c3d38b1c6260c067c12aec5254a49f6c324256c
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-10-07 14:47:54 -0500

    Adding json files generation

--- generator.py
+++ generator.py
@@ -7,5 +7,5 @@ class Generator():
         super().__init__()
 
     def create_json(self, id_property, json_data):
-        with open('property_' + str(id_property) + '.json', 'w') as outfile:
+        with open('./generated_json/property_' + str(id_property) + '.json', 'w') as outfile:
             json.dump(json_data, outfile, indent = 4)
