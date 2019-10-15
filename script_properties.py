8e9fb74749737e5b90052f78f30e95929ad302d2
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-10-14 22:02:57 -0500

    Updating Airflow Scheduler

--- script_properties.py
+++ script_properties.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
-import requests, re
+import requests, re, secrets
 
 def request_properties(properties_pages_list, **kwargs):
-    paginator_url = 'https://www.fincaraiz.com.co/finca-raiz/venta/medellin/?ad=30%7C1%7C%7C%7C%7C1%7C%7C8,9,3,4,22,2,5,7,19,23,21,18,20%7C%7C%7C55%7C5500006%7C%7C%7C%7C%7C%7C%7C%7C%7C%7C%7C%7C%7C%7C%7C%7C1%7C%7C%7C1%7C%7Cgriddate%20desc%7C%7C%7C%7C-1%7C%7C'
+    paginator_url = secrets.MAIN_PAGE_URL
     page = requests.get(paginator_url)
     soup = BeautifulSoup(page.content, 'html.parser')
     properties_number = soup.find(id='lblNumInm').get_text()
@@ -16,6 +16,6 @@ def request_properties(properties_pages_list, **kwargs):
         else:
             property = properties.find('li', class_='media').attrs.get('onclick')
         url_property = str(property).replace('javascript:window.location=', '')
-        url_property = 'https://www.fincaraiz.com.co' + url_property.replace("'", "")
+        url_property = secrets.MAIN_URL + url_property.replace("'", "")
         properties_pages_list.append(url_property)
     return properties_pages_list
