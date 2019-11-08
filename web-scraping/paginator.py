670f22ac9283755b79bc455e55e3100293b7e970
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-11-08 16:25:30 -0500

    Changing paginator

--- web-scraping/paginator.py
+++ web-scraping/paginator.py
@@ -24,8 +24,8 @@ def paginate_properties():
     for script in soup_rent.find_all("script", type="text/javascript"):
         if(pattern.findall(script.text)):
             total_pages_rent = int((pattern.findall(script.text)[0].split()[-1])[0:-1])
-    for i in range(1, int(total_pages_buy / 2) + 1):
+    for i in range(int(total_pages_buy / 2) + 2, total_pages_buy + 1):
         pages_list_to_process.append(secrets.MAIN_PAGE_SHORT_URL_BUY + '/?ad=30|'+ str(i) +'||||1||8,9,3,4,22,2,5,7,19,23,21,18,20|||55|5500006||||||||||||||||1|||1||griddate%20desc||||-1||')
-    for i in range(1, int(total_pages_rent / 2) + 1):
+    for i in range(int(total_pages_rent / 2) + 2, total_pages_rent + 1):
         pages_list_to_process.append(secrets.MAIN_PAGE_SHORT_URL_RENT + '/?ad=30|'+ str(i) +'||||2||8,9,3,4,22,2,5,7,19,23,21,18,20|||55|5500006||||||||||||||||1|||1||griddate%20desc||||-1||')
     return pages_list_to_process
