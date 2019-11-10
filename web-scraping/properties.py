4aa7451a2abeb726ad584fc5af13138a932cbf26
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-11-10 17:05:53 -0500

    Data Cleaning for small data

--- web-scraping/properties.py
+++ web-scraping/properties.py
@@ -9,12 +9,7 @@ def request_properties(pages_list):
         division = soup.find(id='divAdverts')
         for i in range(0, 34):
             properties = division.find(id='rowIndex_' + str(i))
-            property = ''
-            if (i == 0):
-                property = properties.find('li', class_='media')
-                property = property.find('span').attrs.get('onclick')
-            else:
-                property = properties.find('li', class_='media').attrs.get('onclick')
+            property = properties.find('li', class_='media').attrs.get('onclick')
             url_property = str(property).replace('javascript:window.location=', '')
             url_property = secrets.MAIN_URL + url_property.replace("'", "")
             properties_pages_list.append(url_property)
